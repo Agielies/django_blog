85e8ff4606a944755cc973b9059342765e37d244
Agielies <antonmichaelmeyer@gmail.com>
2023-11-05 11:57:51 +0200

    Add more templates

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author' : 'MichaeM',
+        'title' : 'Blog post 1',
+        'content' : 'First post content',
+        'date_posted' : 'August 27, 2018'
+    },
+    {
+        'author' : 'Corey',
+        'title' : 'Blog post 2',
+        'content' : 'First post content',
+        'date_posted' : 'August 27, 2018'
+    },
+]
+
 def home(request):
 
-    return render(request,'blog/home.html') 
+    context = {
+        'posts' : posts,
+        'title' : 'Michael',
+    }
+    return render(request,'blog/home.html',context) 
 
 def about(request):
 
-    return HttpResponse('<h1> This is the about page </h1>')
+    return render(request, 'blog/about.html') 
